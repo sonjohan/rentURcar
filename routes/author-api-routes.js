2de80a6b021c977f52181203f243218e4b5f5bf8
Neil <neilkosh001@gmail.com>
2018-09-06 14:33:50 -0400

    Authentication

--- routes/author-api-routes.js
+++ routes/author-api-routes.js
@@ -1,6 +1,43 @@
 var db = require("../models");
+var passport = require("../helpers/passport.js");
+
 
 module.exports = function(app) {
+
+app.post("/api/login", passport.authenticate("local"), (req, res) => {
+    res.json("/members");
+})
+
+app.post("/api/signup", (req, res) => {
+  db.User.create({
+      email: req.body.email,
+      password: req.body.password
+  }).then(() => {
+      res.redirect(307, "/api/login");
+  }).catch(function (err) {
+      console.log(err);
+      res.json(err);
+  });
+});
+
+app.get("/api/userData", (req, res) => {
+  if (!req.user) {
+      res.json({ "message": "unauth acess" });
+  }
+  else {
+      res.json({
+          email: req.user.email,
+          id: req.user.id
+      });
+  }
+});
+
+app.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
+
+
   app.get("/api/authors", function(req, res) {
     // Here we add an "include" property to our options in our findAll query
     // We set the value to an array of the models we want to include in a left outer join
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -13,18 +13,26 @@ module.exports = function(app) {
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
   // index route loads view.html
-  app.get("/", function(req, res) {
+
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname, "../public/signup.html"))
+});
+
+
+app.get("/login", function (req, res) {
+res.sendFile(path.join(__dirname, "../public/login.html"))
+});
+
+app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
   // cms route loads cms.html
-  app.get("/cms", function(req, res) {
+app.get("/cms", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/cms.html"));
   });
 
   // authors route loads author-manager.html
-  app.get("/signup", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
-  });
+  
 
 };
